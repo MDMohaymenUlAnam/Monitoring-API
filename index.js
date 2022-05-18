8282162eec566ce65597873cfaa99265d8f75e5d
mohaymen <mohaymen2@iut-dhaka.edu>
2022-05-19 00:13:11 +0600

    read and write data on file system

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const http = require('http');
 
 const {handleReqRes} = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
+
+
 const app = {};
+
+data.read('test', 'newfile',  (err,result) =>{
+    console.log(err,result);
+});
 
 
 
