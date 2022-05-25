4d03211782c05e97b4f5f7b5a8defdbbb762c5f8
mohaymen <mohaymen2@iut-dhaka.edu>
2022-05-25 12:20:59 +0600

    check POST route

---
+++ handlers/routeHandlers/checkHandler.js
@@ -0,0 +1,101 @@
+const data = require('../../lib/data');
+const  {hash} = require('../../helpers/utilities');
+const  {parseJSON} = require('../../helpers/utilities');
+const tokenHandler = require('./tokenHandler');
+
+const handler = {};
+
+handler.cheeckHandler = (requestProperties, callback) => {
+    const acceptedMethods = ['get','post','put','delete'];
+    if(acceptedMethods.indexOf(requestProperties.method) > -1) {
+        handler._check[requestProperties.method](requestProperties,callback);
+    }else{
+        callback(405);
+    }
+};
+
+handler._check = {};
+
+handler._check.post = (requestProperties, callback) => {
+    // validate inputs
+    let protocol = typeof(requestProperties.body.protocol) === 'string' && ['http', 'https'].indexOf
+    (requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
+    
+    let url = typeof(requestProperties.body.url) === 'string' && requestProperties.body.url.trim().length > 0
+    ? requestProperties.body.url : false;
+
+    let method = typeof(requestProperties.body.method) === 'string' && ['get', 'post', 'put', 'delete'].
+    indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+
+    
+    let successCodes = typeof(requestProperties.body.successCodes) === 'object' && 
+    requestProperties.body.successCodes instanceof Array ? requestProperties.body.successCodes : false;
+
+    let timeoutSeconds = typeof(requestProperties.body.timeoutSeconds) === 'number' && requestProperties.body.
+    timeoutSeconds % 1 === 0 && requestProperties.body.timeoutSeconds >= 1 &&
+    requestProperties.body.timeoutSeconds <= 5 ? requestProperties.body.timeoutSeconds : false;
+
+    if(protocol && url && method && successCodes && timeoutSeconds){
+        let token = typeof(requestProperties.headersObject.token) === 'string' ?
+        requestProperties.headersObject.token : false;
+
+        // lookup the user phone by reading the token
+        data.read('tokens', token, (err,tokenData) =>{
+            if(!err && tokenData){
+                let userPhone = parseJSON(tokenData).phone;
+
+                // lookup the user data
+                data.read('users', userPhone, (err,userData)=>{
+                    if(!err && userData){
+                        tokenHandler._token.verify(token,phone,(tokenIsValid)=>{
+                            if(tokenIsValid){
+                                let userObject = parseJSON(userData);
+                                
+
+                            }else{
+                                callback(403, {
+                                    error : 'Authentication issue',
+                                });
+                            }
+
+                        });
+
+                    }else {
+                        callback(403, {
+                            error : 'user not found',
+                        });
+                    }
+
+                });
+                
+            }else{
+                callback(403, {
+                    error : 'Authentication issue',
+                });
+            }
+
+        });
+
+    }else {
+        callback(400, {
+            error : 'you have a problem in the request',
+        });
+    }
+};
+
+handler._check.get = (requestProperties, callback) => {
+    
+
+};
+
+
+
+handler._check.put = (requestProperties, callback) => {
+    
+};
+
+handler._check.delete = (requestProperties, callback) => {
+
+
+};
+module.exports = handler;
--- routes.js
+++ routes.js
@@ -3,10 +3,12 @@
 const {sampleHandler} = require('./handlers/routeHandlers/samplehandler');
 const {userHandler} = require('./handlers/routeHandlers/userHandler');
 const{tokenHandler} = require('./handlers/routeHandlers/tokenHandler');
+const {checkHandler} = require('./handlers/routeHandlers/checkHandler');
 const routes = {
     'sample': sampleHandler,
     'user': userHandler,
     'token': tokenHandler,
+    'check': checkHandler,
     
 };
 
