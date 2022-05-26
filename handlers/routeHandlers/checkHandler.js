ad7be41e76541689c30231f806758bdf0269cd75
mohaymen <mohaymen2@iut-dhaka.edu>
2022-05-26 14:41:47 +0600

    4 check operations done

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -142,50 +142,241 @@ handler._check.post = (requestProperties, callback) => {
 };
 
 handler._check.get = (requestProperties, callback) => {
-//     const id =
-//     typeof(requestProperties.queryStringObject.id) === 'string' &&
-//     requestProperties.queryStringObject.id.trim().length === 20
-//         ? requestProperties.queryStringObject.id
-//         : false;
+    const id =
+            typeof(requestProperties.querystringObject.id) === 'string' &&
+            requestProperties.querystringObject.id.trim().length === 20
+                ? requestProperties.querystringObject.id: false;
 
-//     if (id) {
-//     // lookup the check
-//         data.read('checks', id, (err, checkData) => {
-//             if (!err && checkData) {
-//                 const token =
-//                     typeof(requestProperties.headersObject.token) === 'string'
-//                     ? requestProperties.headersObject.token
-//                     : false;
+    if(id){
+        // lookup the check
+        data.read('checks', id, (err,checkData) =>{
+            if(!err && checkData){
+                const token =
+                    typeof requestProperties.headersObject.token === 'string'
+                    ? requestProperties.headersObject.token: false;
 
-//                 tokenHandler._token.verify(token,parseJSON(checkData).userPhone,(tokenIsValid) => {
-//                     if (tokenIsValid) {
-//                         callback(200, parseJSON(checkData));
-//                     } else {
-//                         callback(403, {
-//                             error: 'Authentication failure!',
-//                         });
-//                     }
-//                 });
-//             } else {
-//                 callback(500, {
-//                 error: 'You have a problem in your request',
-//             });
-//         }
-//     });
-// } else {
-//     callback(400, {
-//         error: 'You have a problem in your request',
-//     });
-// }
+                    tokenHandler._token.verify(token, parseJSON(checkData).userPhone, (tokenIsValid) => {
+                        if (tokenIsValid){
+                            callback(200,parseJSON(checkData));
+                        }else{
+                            callback(403, {
+                                error: 'authentication failed',
+                            });
+                        }
+                    });
+
+            }else{
+                callback(500, {
+                    error: 'you have a problem in your request',
+                });
+            }
+
+        });
+
+    }else{
+        callback(400, {
+            error: 'you have a problem in your request',
+        });
+    }
+
 };
 
 
 
 handler._check.put = (requestProperties, callback) => {
-    
+    const id =
+        typeof(requestProperties.body.id) === 'string' &&
+        requestProperties.body.id.trim().length === 20
+            ? requestProperties.body.id
+            : false;
+
+    const protocol =
+        typeof requestProperties.body.protocol === 'string' &&
+        ['http', 'https'].indexOf(requestProperties.body.protocol) > -1
+            ? requestProperties.body.protocol
+            : false;
+        
+    const url =
+        typeof requestProperties.body.url === 'string' &&
+        requestProperties.body.url.trim().length > 0
+            ? requestProperties.body.url
+            : false;
+        
+    const method =
+        typeof requestProperties.body.method === 'string' &&
+        ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1
+            ? requestProperties.body.method
+            : false;
+        
+    const successCodes =
+        typeof requestProperties.body.successCodes === 'object' &&
+        requestProperties.body.successCodes instanceof Array
+            ? requestProperties.body.successCodes
+            : false;
+        
+    const timeoutSeconds =
+        typeof requestProperties.body.timeoutSeconds === 'number' &&
+        requestProperties.body.timeoutSeconds % 1 === 0 &&
+        requestProperties.body.timeoutSeconds >= 1 &&
+        requestProperties.body.timeoutSeconds <= 5
+            ? requestProperties.body.timeoutSeconds
+            : false;
+
+    if(id){
+        if(protocol || url || method || successCodes || timeoutSeconds){
+            data.read('checks', id, (err,checkData) =>{
+                if(!err && checkData){
+                    let checkObject = parseJSON(checkData);
+                    const token =
+                        typeof requestProperties.headersObject.token === 'string'
+                            ? requestProperties.headersObject.token
+                            : false;
+
+                    tokenHandler._token.verify(token, checkObject.userPhone, (tokenIsValid) => {
+                        if(tokenIsValid){
+                            if(protocol){
+                                checkObject.protocol = protocol;
+                            };
+                            if(url){
+                                checkObject.url = url;
+                            };
+                            if(method){
+                                checkObject.method = method;
+                            };
+                            if(successCodes){
+                                checkObject.successCodes = successCodes;
+                            };
+                            if(timeoutSeconds){
+                                checkObject.imeoutSeconds = imeoutSeconds;
+                            };
+
+                            data.update('checks', id, checkObject, (err)=>{
+                                if(!err){
+                                    callback(200);
+
+                                }else{
+                                    callback(500, {
+                                        error: 'there was a server error',
+                                    });
+                                }
+
+                            });
+
+                        }else{
+                            callback(403, {
+                                error: 'Authentication error',
+                            });
+                        }
+                    });
+
+
+                }else{
+                    callback(500, {
+                        error: 'server side problem',
+                    });
+                }
+
+            });
+
+        }else{
+            callback(400, {
+                error: 'must provide one field to update',
+            });
+        }
+        
+    }else{
+        callback(400, {
+            error: 'you have a problem in your  request',
+        });
+    }
+        
 };
 
 handler._check.delete = (requestProperties, callback) => {
+    const id =
+            typeof(requestProperties.querystringObject.id) === 'string' &&
+            requestProperties.querystringObject.id.trim().length === 20
+                ? requestProperties.querystringObject.id: false;
+
+    if(id){
+        // lookup the check
+        data.read('checks', id, (err,checkData) =>{
+            if(!err && checkData){
+                const token =
+                    typeof requestProperties.headersObject.token === 'string'
+                    ? requestProperties.headersObject.token: false;
+
+                    tokenHandler._token.verify(token, parseJSON(checkData).userPhone, (tokenIsValid) => {
+                        if (tokenIsValid){
+                        //    delete the check data
+                            data.delete('checks', id,(err)=>{
+                                if(!err){
+                                    data.read('users', parseJSON(checkData).userPhone, (err,userData)=>{
+                                        let userObject = parseJSON(userData);
+                                        if(!err && userData){
+                                            let userChecks = typeof(userObject.checks) === 'object' &&
+                                            userObject.checks instanceof Array ? userObject.checks : [];
+
+                                            // remove the deleted check id from user check list
+                                            let checkPosition = userChecks.indexOf(id);
+                                            if(checkPosition > -1){
+                                                userChecks.splice(checkPosition, 1);
+
+                                                userObject.checks = userChecks;
+                                                data.update('users', userObject.phone, userObject, (err) =>{
+                                                    if(!err){
+                                                        callback(200);
+                                                    }else{
+                                                        callback(500, {
+                                                            error: 'server side problem',
+                                                        });
+                                                    }
+
+                                                });
+                                                
+                                            }else{
+                                                callback(500, {
+                                                    error: 'check id is not found',
+                                                });
+
+                                            }
+
+
+                                        }else{
+                                            callback(500, {
+                                                error: 'server side problem',
+                                            });
+                                        }
+
+                                    });
+                                    
+                                }else{
+                                    callback(500, {
+                                        error: 'server side problem',
+                                    });
+                                }
+
+                            });
+                        }else{
+                            callback(403, {
+                                error: 'authentication failed',
+                            });
+                        }
+                    });
+
+            }else{
+                callback(500, {
+                    error: 'you have a problem in your request',
+                });
+            }
+
+        });
+
+    }else{
+        callback(400, {
+            error: 'you have a problem in your request',
+        });
+    }
 
 
 };
